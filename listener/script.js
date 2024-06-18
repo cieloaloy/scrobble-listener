9924030463833ca741322d8c22673ed1c1fbd926
Cielo Loy <cieloaloy@gmail.com>
2024-06-17 22:28:20 -0700

    Create script.js

---
+++ listener/script.js
@@ -0,0 +1,8 @@
+// event loop
+// play button (active recording)
+// slider to select time interval, x
+// record and cache ten seconds of audio every x seconds
+// pass recording to shazamio
+// if success, cache title
+// if title = same as prev, discard
+// if different, scrobble to lastfm and overwrite prev
