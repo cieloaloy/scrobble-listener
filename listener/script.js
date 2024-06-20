28811a49f07b0eba5aadd5afd0bb16a09e58fff9
cieloaloy <cieloaloy@gmail.com>
2024-06-20 16:46:52 -0700

    Add html

--- listener/script.js
+++ listener/script.js
@@ -1,8 +1,37 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function startRecording() {
+    document.getElementById('status').innerHTML = 'Actively Listening';
+    active = true;
+    // record and cache ten seconds of audio every x seconds
+    //while (active) {
+    for (let i = 0; i<10; i++) {
+        console.log("guh");
+        await sleep(2000);
+    // pass recording to shazamio
+    // if success, cache title
+    // if title = same as prev, discard
+    // if different, scrobble to lastfm and overwrite prev
+    }
+}
+
+function stopRecording() {
+    document.getElementById('status').innerHTML = '';
+    active = false;
+}
+
 // event loop
-// play button (active recording)
-// slider to select time interval, x
-// record and cache ten seconds of audio every x seconds
-// pass recording to shazamio
-// if success, cache title
-// if title = same as prev, discard
-// if different, scrobble to lastfm and overwrite prev
+document.addEventListener('DOMContentLoaded', () => {
+    var active = false;
+    // slider to select time interval, x
+
+    // play button (active recording)
+    document.getElementById('recorder').addEventListener('click', () => {
+        startRecording();
+    });
+    document.getElementById('stopper').addEventListener('click', () => {
+        stopRecording();
+    });
+});
